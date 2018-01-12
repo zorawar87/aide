791c941d3e4950247a30dcc51781f4fbf9428d04
Zorawar Moolenaar <zorawar87@gmail.com>
2018-01-12 15:00:31 -0500

    improvements

--- aide.py
+++ aide.py
@@ -6,7 +6,9 @@ import sys
 from bs4 import BeautifulSoup as bs
 import time
 import json
+import logging
 
+logging.basicConfig(level=logging.INFO)
 parser = argparse.ArgumentParser()
 parser.add_argument('username', help="your mytrinnet username")
 parser.add_argument('password', help="your mytrinnet password")
@@ -14,17 +16,18 @@ parser.add_argument('ll', type=int, help="lower limit of the iterations")
 parser.add_argument('ul', type=int, help="upper limit of the iterations")
 
 totalis = []
+exceptions = []
 
 def main(ns):
     #with Browser('chrome', headless=True) as browser:
     with Browser('chrome') as browser:
-        print("successful authentication! iterating profiles...")
         login(browser)
         time.sleep(2)
         iterateProfiles(browser, ns.ll, ns.ul)
+    writeExceptions()
 
 def login(browser):
-    print("starting...")
+    logging.info("attempting to log in...")
     url = "https://securelb.imodules.com/s/1490/index-3Col.aspx?sid=1490&gid=1&pgid=3&cid=40"
     browser.visit(url)
     browser.fill('cid_40$txtUsername', ns.username)
@@ -32,19 +35,15 @@ def login(browser):
     browser.find_by_name('cid_40$btnLogin').click()
 
 def iterateProfiles(browser, low, high):
+    logging.info("iterating profiles...")
     global totalis
     for i in range(low, high):
         url = "https://mytrinnet.trincoll.edu/s/1490/index-3Col.aspx?sid=1490&gid=1&pgid=275&cid=735&mid="+ str(i) +"#/PersonalProfile"
         browser.visit(url)
         time.sleep(0.5)
-        if parseHTML(i,browser.html) == False:
-            appendJSON()
-            totalis =[]
-            print("emptied array")
-            print(totalis)
+        verifyPerson(parseHTMLToPerson(i,browser.html))
     
-def parseHTML(mid, html):
-    global totalis
+def parseHTMLToPerson(mid, html):
     page = bs(html, 'html.parser')
     person = {}
     person.update({"mid":mid})
@@ -61,14 +60,29 @@ def parseHTML(mid, html):
                 person.update(kv_pair)
                 label=""
                 data=""
-    totalis.append(person)
-    print("%d person has %d keys." % (mid, len(person)) )
+    return person
+
+def verifyPerson(person):
+    global totalis
+    mid = person["mid"]
+    logging.info("%d person has %d keys." % (mid, len(person)) )
     if len(person) == 1:
+        logging.warning("Error or Blank ID at %d" % mid)
         appendJSON()
+        totalis = []
+        exceptions.append(mid)
+    else:
+        totalis.append(person)
 
 def appendJSON():
     with open("out.json","a") as f:
         json.dump(totalis, f)
+        f.write("\n")
+
+def writeExceptions():
+    with open("exceptions.csv","a") as f:
+        for index in exceptions:
+            f.write("%d, " % index)
         f.write("\n")
 
 if __name__ == "__main__":
@@ -76,6 +90,6 @@ if __name__ == "__main__":
     ns = parser.parse_args(sys.argv[1:])
     main(ns)
     end = time.time()
-    print("Iterating over [%d, %d) took: %f seconds" %(ns.ll, ns.ul, end-start))
+    logging.info("Iterating over [%d, %d) took: %f seconds" %(ns.ll, ns.ul, end-start))
 
 
