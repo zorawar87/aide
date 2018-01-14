37354cde8a13a40b35277e6a459241a815ab8f0b
Zorawar Moolenaar <zorawar87@gmail.com>
2018-01-14 11:06:02 -0500

    utility to sanitise N files

---
+++ santise.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Utility to uniquely sort (sanitise) N files
+# Usage: ./sanitise <file1 file2 ...>
+# BUG: the true echo line should not print the name of $var
+
+for var in "$@"
+do
+  if [ -f "$var" ]; then
+    sort -u "$var" -o "$var"
+    echo "sorted uniquely into" $(wc -l "$var") "lines"
+  else
+    echo "$var is an invalid file"
+    exit 1
+  fi
+done
+
